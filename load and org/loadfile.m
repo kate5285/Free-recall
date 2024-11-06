d19d1c7d8bf584f8f7152a0265f40348182b49fd
Doyeon Kim <kate5285@snu.ac.kr>
2024-11-06 18:55:06 +0900

    Update loadfile.m

--- load and org/loadfile.m
+++ load and org/loadfile.m
@@ -11,3 +11,14 @@ wfilename= "C:\Users\Leelab_Intern\Desktop\doyeon kim\free recall\recallwords.xl
 recallwords = readtable(wfilename, 'Range', sprintf('A1:H10'));
 kfilename= "C:\Users\Leelab_Intern\Desktop\doyeon kim\free recall\kortoeng.xlsx"; % file directory for word list shown to participants (in English)
 ktoengwords = readtable(kfilename, 'Range', sprintf('A1:H10'));
+
+%load sbj info
+filename= "C:\Users\Leelab_Intern\Desktop\doyeon kim\free recall\www_info_DY.xlsx";
+partici =readcell(filename, 'Sheet', 1, 'Range','A:A');
+numRows = numel(partici)-1;
+VariableNames = readcell(filename, 'Range', 'A1:G1');
+info = readtable(filename, 'Range', sprintf('A2:G%d',numRows+1));
+info.Properties.VariableNames = VariableNames;
+age=info{:,3};
+mmse=info{:,7};
+edu=info{:,4};
