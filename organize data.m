01b8980d4973f3495b5d03d799e9580573bf187f
Doyeon Kim <kate5285@snu.ac.kr>
2024-11-06 18:16:30 +0900

    Update organize data.m

--- organize data.m
+++ organize data.m
@@ -1,3 +1,37 @@
+%organize data
+subjects = unique(recall{:,'SBJ #'});
+Alldata = cell(length(subjects), 1);
+for i = 1:length(subjects)
+    subj_data = recall(strcmp(recall{:,'SBJ #'}, subjects{i}), :);
+    blockn = unique(subj_data{:,'Trial (BLOCK) #'});
+    blocks = cell(length(blockn), 1);
+    for j = 1:length(blockn)
+        block_data = subj_data(subj_data{:,'Trial (BLOCK) #'} == blockn(j), :);
+        trial_struct = struct();
+        trial_struct.sbj=block_data{:,'SBJ #'}{1};
+
+        trial_struct.ansnum = block_data{:,'Answer #'};
+
+        if any(trial_struct.ansnum == 0)
+        trial_struct.allansnum = numel(trial_struct.ansnum) - 1;
+        else
+        disp("wrong ans 0");
+        end
+
+        trial_struct.answord = block_data.word;
+        trial_struct.wrongword = block_data{:,'wrong word'};
+        trial_struct.anstime = block_data.time;
+
+        num_wrong_words = sum(~cellfun(@isempty, trial_struct.wrongword)); % 오답 단어 수
+        trial_struct.ransnum = trial_struct.allansnum - num_wrong_words;
+   
+        trial_struct.serial = block_data{:,'Trial (BLOCK) #'}(1,1);
+        blocks{j} = trial_struct;
+    end
+    Alldata{i} = blocks;
+end
+numSubjects = length(Alldata);
+
 numSubjects = length(Alldata);
 for i = 1:numSubjects
     trials = Alldata{i, 1};
